e49ab8090ba81889510f13d261ed3d34c6d8de72
xiangyun.kong <kongxiangyun@yxqiche.com>
2019-08-26 10:04:34 +0800

    清楚spring mvc以来

--- src/main/java/com/kxy/annotation/KXYController.java
+++ src/main/java/com/kxy/annotation/KXYController.java
@@ -1,6 +1,5 @@
 package com.kxy.annotation;
 
-import org.springframework.stereotype.Indexed;
 
 import java.lang.annotation.*;
 
@@ -10,7 +9,6 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Indexed
 public @interface KXYController {
 
     String value() default "";
